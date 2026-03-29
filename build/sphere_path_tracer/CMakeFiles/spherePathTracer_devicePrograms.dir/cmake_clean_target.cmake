482acecc78f7d160595441760ba18989b90d072d
Mae Pigeon <maepigeon1217@gmail.com>
2026-03-29 14:41:29 -0400

    removed build

--- build/sphere_path_tracer/CMakeFiles/spherePathTracer_devicePrograms.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "../libspherePathTracer_devicePrograms.a"
-)
--- build/sphere_path_tracer/CMakeFiles/spherePathTracer_devicePrograms_ptx.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for spherePathTracer_devicePrograms_ptx.
